b3f22a1e7abe474e5ec997efb40ed009d8da7934
Horsen121 <vova.mitychin@mail.ru>
2022-11-20 23:05:32 +0300

    Upd bot

--- RPBD/buy_list/api/api.go
+++ RPBD/buy_list/api/api.go
@@ -7,7 +7,7 @@ import (
 var Buttons = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("Add at list"),
-		tgbotapi.NewKeyboardButton("Add at refrigirator"),
+		tgbotapi.NewKeyboardButton("Add at refrigerator"),
 		tgbotapi.NewKeyboardButton("Open product"),
 	),
 	tgbotapi.NewKeyboardButtonRow(
@@ -15,5 +15,44 @@ var Buttons = tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButton("Product list"),
 		tgbotapi.NewKeyboardButton("Last products"),
 		tgbotapi.NewKeyboardButton("Stats"),
+	),
+)
+
+var AddToList = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Name", "name"),
+		tgbotapi.NewInlineKeyboardButtonData("Weight", "weight"),
+		tgbotapi.NewInlineKeyboardButtonData("Notice", "notice"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Add", "ToList"),
+	),
+)
+var AddToRefrigerator = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("From list", "query"), /// query to Product list
+		tgbotapi.NewInlineKeyboardButtonData("Another", "product"),
+		tgbotapi.NewInlineKeyboardButtonData("Time", "time"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Add", "ToRefrigerator"),
+	),
+)
+var OpenProduct = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Product", "product"), /// query to Product list
+		tgbotapi.NewInlineKeyboardButtonData("New time", "time"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Open", "Open"),
+	),
+)
+var ChangeStatus = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Product", "product"), /// query to Product list
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Done", "Done"),
+		tgbotapi.NewInlineKeyboardButtonData("Cast", "Cast"),
 	),
 )
--- RPBD/buy_list/bot/bot.go
+++ RPBD/buy_list/bot/bot.go
@@ -25,23 +25,45 @@ func Start() {
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
 			msg.ReplyMarkup = api.Buttons
 
 			switch update.Message.Text {
 			case "Add at list":
-			case "Add at refrigirator":
+				msg.Text = "Enter the name of the product, its weight and the time of the purchase reminder (if necessary)"
+				msg.ReplyMarkup = api.AddToList
+			case "Add at refrigerator":
+				msg.Text = "Enter the name of the product (or select from the list) and its expiration date"
+				msg.ReplyMarkup = api.AddToRefrigerator
 			case "Open product":
+				msg.Text = "Select product from the list and its new expiration date"
+				msg.ReplyMarkup = api.OpenProduct
 			case "Change status":
+				msg.Text = "Select product from the list and select its status"
+				msg.ReplyMarkup = api.ChangeStatus
 			case "Product list":
+				msg.Text = "" // query
 			case "Last products":
+				msg.Text = "" // query
 			case "Stats":
+				msg.Text = "" // query
 			}
 
 			bot.Send(msg)
+		} else if update.CallbackQuery != nil {
+			// Respond to the callback query, telling Telegram to show the user
+			// a message with the data received.
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.Request(callback); err != nil {
+				panic(err)
+			}
+
+			// And finally, send a message containing the data received.
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
 		}
 	}
 }
